dde2bd4be1c4b333a8805090a5844b307670a14a
rhamza783 <70859024+rhamza783@users.noreply.github.com>
2025-08-24 00:41:09 +0500

    Create authMiddleware.js

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: '../../.env' });
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'No token provided, authorization denied.' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Add user payload to the request object
+    next(); // Proceed to the next middleware or route handler
+  } catch (error) {
+    res.status(401).json({ error: 'Token is not valid.' });
+  }
+};
+
+module.exports = { authMiddleware };
