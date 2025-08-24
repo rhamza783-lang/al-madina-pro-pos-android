5ddf4e26858b1c14551aedda4a5518bb3508f282
rhamza783 <70859024+rhamza783@users.noreply.github.com>
2025-08-24 22:57:38 +0500

    Create Color.kt

---
+++ android-app/app/src/main/java/com/almadina/pos/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.almadina.pos.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFF006A4E)
+val SecondaryColor = Color(0xFF4ECDC4)
+val BackgroundColor = Color(0xFFF0F4F8)
+
+val TextPrimary = Color(0xFF2E3A59)
+val TextSecondary = Color(0xFF8F9BB3)
+
+val SuccessColor = Color(0xFF28A745)
+val ErrorColor = Color(0xFFDC3545)
+val WarningColor = Color(0xFFFFC107)
+val InfoColor = Color(0xFF17A2B8)
+
+val BorderColor = Color(0xFFE4E9F2)
