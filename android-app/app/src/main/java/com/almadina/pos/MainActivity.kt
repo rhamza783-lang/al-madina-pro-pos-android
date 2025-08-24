5e00e6cc8cf68088dcc153f5aef087574cacf189
rhamza783 <70859024+rhamza783@users.noreply.github.com>
2025-08-24 22:55:16 +0500

    Create MainActivity.kt

---
+++ android-app/app/src/main/java/com/almadina/pos/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.almadina.pos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.almadina.pos.ui.navigation.AppNavigation
+import com.almadina.pos.ui.theme.AlMadinaPOSTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AlMadinaPOSTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
