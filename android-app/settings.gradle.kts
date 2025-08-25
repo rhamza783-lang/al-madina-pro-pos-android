989625eb06850e14a58046543dd08ce29bf7533d
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-25 23:51:42 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -4,20 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    plugins {
-        // Declare Android Application Plugin with version
-        id("com.android.application") version "8.0.2"
-    }
 }
 
-// Include your app module
-include(":app")
-
-// Optional: If you have other modules
-// include(":feature:login")
-// include(":core")
-
-// If using Kotlin, also declare Kotlin plugin
-// plugins {
-//     id("org.jetbrains.kotlin.android") version "1.9.0"
-// }
+include(":app"
