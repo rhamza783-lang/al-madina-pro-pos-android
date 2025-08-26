44f6dd2b3800040afd143d2ad39e7773fa12b91d
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-26 11:33:45 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -1,12 +1,27 @@
+// This top block is for defining the versions of the BUILD PLUGINS
 pluginManagement {
+    // This tells Gradle where to find the plugins themselves
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+    // This is the new, crucial part. We define the plugin versions here.
     plugins {
-        id("com.android.application") version "8.0.2"
+        id("com.android.application") version "8.2.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+        id("com.google.dagger.hilt.android") version "2.48" apply false
     }
 }
 
+// This block is for defining the versions of your APP LIBRARIES
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AlMadinaPOS"
 include(":app")
