a88e82557c427edccc67b43521b9d05e14b8cfb0
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-26 12:15:24 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -1,21 +1,18 @@
-// This top block is for defining the versions of the BUILD PLUGINS
 pluginManagement {
-    // This tells Gradle where to find the plugins themselves
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
-    // This is the new, crucial part. We define the plugin versions here.
     plugins {
         id("com.android.application") version "8.2.2" apply false
         id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-        id("org.jetbrains.kotlin.kapt") version "1.8.10" apply false // <-- THIS LINE IS THE FIX
+        id("org.jetbrains.kotlin.kapt") version "1.8.10" apply false
         id("com.google.dagger.hilt.android") version "2.48" apply false
+        id("org.jetbrains.kotlin.plugin.parcelize") version "1.8.10" apply false // <-- FINAL PLUGIN ADDED
     }
 }
 
-// This block is for defining the versions of your APP LIBRARIES
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
