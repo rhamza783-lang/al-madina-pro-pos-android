aafc7b47e86c2d5b626fe38be24435214224d2ed
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-26 23:21:50 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -5,22 +5,8 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.2.2" apply false
-        id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-        id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
-        id("com.google.dagger.hilt.android") version "2.48" apply false
-        id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.22" apply false
+        id("com.android.application") version "8.2.2"
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
-rootProject.name = "AlMadinaPOS"
 include(":app")
