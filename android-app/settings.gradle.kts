39ca36cc14f7c2a6cdd2d8c249b7b32a3d38e16e
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-26 14:58:21 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -6,7 +6,6 @@ pluginManagement {
     }
     plugins {
         id("com.android.application") version "8.2.2" apply false
-        // --- THIS IS THE FINAL FIX ---
         id("org.jetbrains.kotlin.android") version "1.9.22" apply false
         id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
         id("com.google.dagger.hilt.android") version "2.48" apply false
