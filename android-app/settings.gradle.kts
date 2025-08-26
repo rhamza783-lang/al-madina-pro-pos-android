67c43c36260f6812aa5d4bdb33dc919a052ff1c9
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-26 23:27:53 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
     }
     plugins {
         id("com.android.application") version "8.2.2"
+        id("org.jetbrains.kotlin.android") version "1.9.0" // ✅ Add this line
     }
 }
 
