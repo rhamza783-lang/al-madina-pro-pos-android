be10c12ba3dc2118e9bf7034fdd8785714f2d76c
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-27 17:37:33 +0500

    Update Item.kt

--- android-app/app/src/main/java/com/almadina/pos/model/Item.kt
+++ android-app/app/src/main/java/com/almadina/pos/model/Item.kt
@@ -4,11 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.almadina.pos.data.local.Converters
-import kotlinx.parcelize.Parcelize
-import android.os.Parcelable
 
-// Item Entity
-@Parcelize
 @Entity(tableName = "items")
 @TypeConverters(Converters::class)
 data class Item(
@@ -19,30 +15,24 @@ data class Item(
     val price: Double,
     val isAvailable: Boolean = true,
     val modifierGroups: List<ModifierGroup> = emptyList()
-) : Parcelable
+)
 
-// Modifier Group (part of Item)
-@Parcelize
 data class ModifierGroup(
     val id: String,
     val name: String,
     val modifiers: List<Modifier>
-) : Parcelable
+)
 
-// Modifier (leaf node)
-@Parcelize
 data class Modifier(
     val id: String,
     val name: String,
     val price: Double = 0.0
-) : Parcelable
+)
 
-// Category Entity
-@Parcelize
 @Entity(tableName = "categories")
 @TypeConverters(Converters::class)
 data class Category(
     @PrimaryKey
     val id: String,
     val name: String
-) : Parcelable
+)
