ae75b12a1e9020060eb70581b9dd0b273b28986f
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-27 13:49:56 +0500

    Update User.kt

--- android-app/app/src/main/java/com/almadina/pos/model/User.kt
+++ android-app/app/src/main/java/com/almadina/pos/model/User.kt
@@ -2,11 +2,14 @@ package com.almadina.pos.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.almadina.pos.data.local.Converters
 import kotlinx.parcelize.Parcelize
 import android.os.Parcelable
 
 @Parcelize
 @Entity(tableName = "users")
+@TypeConverters(Converters::class) // ✅ Add this
 data class User(
     @PrimaryKey val id: String,
     val staffId: String,
