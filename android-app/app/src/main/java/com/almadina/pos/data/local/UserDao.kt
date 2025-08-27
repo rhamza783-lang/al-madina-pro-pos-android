753220b9e19445a472aa8fd828c135c695e27490
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-27 23:02:16 +0500

    Update UserDao.kt

--- android-app/app/src/main/java/com/almadina/pos/data/local/UserDao.kt
+++ android-app/app/src/main/java/com/almadina/pos/data/local/UserDao.kt
@@ -1,11 +1,11 @@
 package com.almadina.pos.data.local
 
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 import com.almadina.pos.model.User
 
 @Dao
 interface UserDao {
-    @Query("SELECT * FROM users WHERE staffId = :staffId LIMIT 1")
+
+    @Query("SELECT * FROM users WHERE staffId = :staffId")
     suspend fun getUserByStaffId(staffId: String): User?
 }
