608d688b6abd49d72b1b26fdbf60fe3d627ec44a
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-27 13:52:33 +0500

    Update Converters.kt

--- android-app/app/src/main/java/com/almadina/pos/data/local/Converters.kt
+++ android-app/app/src/main/java/com/almadina/pos/data/local/Converters.kt
@@ -1,45 +1,107 @@
 package com.almadina.pos.data.local
 
 import androidx.room.TypeConverter
-import com.almadina.pos.model.ItemModifier
-import com.almadina.pos.model.ModifierGroup
-import com.almadina.pos.model.OrderItem
-import com.almadina.pos.model.Payment
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.almadina.pos.model.*
 
 class Converters {
     private val gson = Gson()
 
+    // === ModifierGroup (List<ModifierGroup>) ===
     @TypeConverter
-    fun fromOrderItemList(value: List<OrderItem>?): String = gson.toJson(value)
-
-    @TypeConverter
-    fun toOrderItemList(value: String): List<OrderItem>? {
-        return gson.fromJson(value, object : TypeToken<List<OrderItem>?>() {}.type)
-    }
-    
-    @TypeConverter
-    fun fromPaymentList(value: List<Payment>?): String = gson.toJson(value)
-
-    @TypeConverter
-    fun toPaymentList(value: String): List<Payment>? {
-        return gson.fromJson(value, object : TypeToken<List<Payment>?>() {}.type)
+    fun fromModifierGroups(modifierGroups: List<ModifierGroup>?): String? {
+        return modifierGroups?.let { gson.toJson(it) }
     }
 
     @TypeConverter
-    fun fromItemModifierList(value: List<ItemModifier>?): String = gson.toJson(value)
+    fun toModifierGroups(json: String?): List<ModifierGroup>? {
+        return json?.let {
+            val type = object : TypeToken<List<ModifierGroup>>() {}.type
+            gson.fromJson(it, type)
+        }
+    }
 
+    // === OrderItem (List<OrderItem>) ===
     @TypeConverter
-    fun toItemModifierList(value: String): List<ItemModifier>? {
-        return gson.fromJson(value, object : TypeToken<List<ItemModifier>?>() {}.type)
+    fun fromOrderItems(items: List<OrderItem>?): String? {
+        return items?.let { gson.toJson(it) }
     }
 
     @TypeConverter
-    fun fromModifierGroupList(value: List<ModifierGroup>?): String = gson.toJson(value)
+    fun toOrderItems(json: String?): List<OrderItem>? {
+        return json?.let {
+            val type = object : TypeToken<List<OrderItem>>() {}.type
+            gson.fromJson(it, type)
+        }
+    }
+
+    // === Payment (List<Payment>) ===
+    @TypeConverter
+    fun fromPayments(payments: List<Payment>?): String? {
+        return payments?.let { gson.toJson(it) }
+    }
 
     @TypeConverter
-    fun toModifierGroupList(value: String): List<ModifierGroup>? {
-        return gson.fromJson(value, object : TypeToken<List<ModifierGroup>?>() {}.type)
+    fun toPayments(json: String?): List<Payment>? {
+        return json?.let {
+            val type = object : TypeToken<List<Payment>>() {}.type
+            gson.fromJson(it, type)
+        }
+    }
+
+    // === PaymentStatus (enum) ===
+    @TypeConverter
+    fun fromPaymentStatus(status: PaymentStatus?): String? {
+        return status?.name
+    }
+
+    @TypeConverter
+    fun toPaymentStatus(name: String?): PaymentStatus? {
+        return name?.let { PaymentStatus.valueOf(it) }
+    }
+
+    // === OrderStatus (enum) ===
+    @TypeConverter
+    fun fromOrderStatus(status: OrderStatus?): String? {
+        return status?.name
+    }
+
+    @TypeConverter
+    fun toOrderStatus(name: String?): OrderStatus? {
+        return name?.let { OrderStatus.valueOf(it) }
+    }
+
+    // === PaymentMethod (enum) ===
+    @TypeConverter
+    fun fromPaymentMethod(method: PaymentMethod?): String? {
+        return method?.name
+    }
+
+    @TypeConverter
+    fun toPaymentMethod(name: String?): PaymentMethod? {
+        return name?.let { PaymentMethod.valueOf(it) }
+    }
+
+    // === TableStatus (enum) ===
+    @TypeConverter
+    fun fromTableStatus(status: TableStatus?): String? {
+        return status?.name
+    }
+
+    @TypeConverter
+    fun toTableStatus(name: String?): TableStatus? {
+        return name?.let { TableStatus.valueOf(it) }
+    }
+
+    // === UserRole (enum) ===
+    @TypeConverter
+    fun fromUserRole(role: UserRole?): String? {
+        return role?.name
+    }
+
+    @TypeConverter
+    fun toUserRole(name: String?): UserRole? {
+        return name?.let { UserRole.valueOf(it) }
     }
 }
