7651b6be27ad89dc86a8dbf621fb9eb031eaa6a1
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-28 09:43:23 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -5,10 +5,10 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.2.2"
-        id("org.jetbrains.kotlin.android") version "1.9.0"
-        id("org.jetbrains.kotlin.kapt") version "1.9.0"
-        id("com.google.dagger.hilt.android") version "2.56.2" // ✅ Add Hilt plugin
+        id("com.android.application") version "8.2.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+        id("org.jetbrains.kotlin.kapt") version "1.9.0" apply false
+        id("com.google.dagger.hilt.android") version "2.56.2" apply false
     }
 }
 
