80ddafd376653f418f6c49f50045e0f758348b63
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-29 01:54:12 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -7,9 +7,11 @@ pluginManagement {
     plugins {
         id("com.android.application") version "8.7.0" apply false
         id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-        id("org.jetbrains.kotlin.kapt") version "2.0.20" apply false
+        id("org.jetbrains.kotlin.kapt") version "2.0.20" apply false // Still needed for some Java annotations
         id("com.google.dagger.hilt.android") version "2.56.2" apply false
         id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+        // ✅ Add KSP plugin
+        id("com.google.devtools.ksp") version "2.0.20-1.0.27" apply false
     }
 }
 
