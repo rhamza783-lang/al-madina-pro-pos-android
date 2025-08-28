f77802e967aff5ae880b826f79bfcb89e95f46fa
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-28 11:39:52 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -5,10 +5,25 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.3.0" // ✅ Upgrade to 8.3.0
-        id("org.jetbrains.kotlin.android") version "1.9.24" // ✅ Use latest 1.9.x
-        id("org.jetbrains.kotlin.kapt") version "1.9.24"
-        id("com.google.dagger.hilt.android") version "2.56.2"
+        id("com.android.application") version "8.2.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+        id("org.jetbrains.kotlin.kapt") version "1.9.0" apply false
+        id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    }
+}
+
+// ✅ Force Kotlin version
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        libs {
+            version("kotlin", "1.9.0")
+            library("kotlin-stdlib").to("org.jetbrains.kotlin", "kotlin-stdlib").version("kotlin")
+            library("kotlin-test").to("org.jetbrains.kotlin", "kotlin-test").version("kotlin")
+        }
     }
 }
 
