dadba0256b01242caa9ad53308c488cd7d3e6817
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-28 14:47:57 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -9,7 +9,6 @@ pluginManagement {
         id("org.jetbrains.kotlin.android") version "2.0.20" apply false
         id("org.jetbrains.kotlin.kapt") version "2.0.20" apply false
         id("com.google.dagger.hilt.android") version "2.56.2" apply false
-        // ✅ Required for Kotlin 2.0+ with Compose
         id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
     }
 }
