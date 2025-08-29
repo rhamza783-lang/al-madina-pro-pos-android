c514f0accf11c689707e93a61b5895a4be77a065
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-29 13:16:29 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -5,11 +5,16 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        // Define all our plugins with compatible versions
-        id("com.android.application") version "8.5.0" apply false
-        id("org.jetbrains.kotlin.android") version "1.9.23" apply false // Use a stable KSP-compatible version
-        id("com.google.dagger.hilt.android") version "2.51.1" apply false // Hilt version compatible with KSP
-        id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false // <-- KSP PLUGIN ADDED
+        // A stable, widely-used version of the Android Gradle Plugin
+        id("com.android.application") version "8.2.2" apply false
+        // The exact Kotlin version compatible with our Compose Compiler
+        id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+        // The stable Kapt plugin for the same Kotlin version
+        id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+        // A stable version of Hilt
+        id("com.google.dagger.hilt.android") version "2.48" apply false
+        // THE MISSING PARCELIZE PLUGIN
+        id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.22" apply false
     }
 }
 
