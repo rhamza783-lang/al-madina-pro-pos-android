292c010a87e58ab880b360a7f23393cdd4a487df
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-29 14:27:09 +0500

    Update settings.gradle.kts

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -5,27 +5,11 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        // A stable, widely-used version of the Android Gradle Plugin
-        id("com.android.application") version "8.2.2" apply false
-        // The exact Kotlin version compatible with our Compose Compiler
-        id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-        // The stable Kapt plugin for the same Kotlin version
-        id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
-        // A stable version of Hilt
-        id("com.google.dagger.hilt.android") version "2.48" apply false
-        // THE MISSING PARCELIZE PLUGIN
-        id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.22" apply false
+        id("com.android.application") version "8.5.0" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+        id("com.google.dagger.hilt.android") version "2.51.1" apply false
+        id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
+        id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.23" apply false
     }
 }
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
-rootProject.name = "AlMadinaPOS"
-include(":app")
+// ... (rest of the file is the same)
