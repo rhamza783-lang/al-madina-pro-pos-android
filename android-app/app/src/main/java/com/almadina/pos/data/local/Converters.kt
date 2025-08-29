7c1f2f1558fc8e8ccfb3c367cba3158e4c4d117f
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-29 14:30:17 +0500

    Update Converters.kt

--- android-app/app/src/main/java/com/almadina/pos/data/local/Converters.kt
+++ android-app/app/src/main/java/com/almadina/pos/data/local/Converters.kt
@@ -9,30 +9,26 @@ class Converters {
     private val gson = Gson()
 
     // --- ENUM CONVERTERS ---
-    @TypeConverter
-    fun fromSyncStatus(status: SyncStatus?): String? = status?.name
-    @TypeConverter
-    fun toSyncStatus(value: String?): SyncStatus? = value?.let { SyncStatus.valueOf(it) }
+    @TypeConverter fun fromSyncStatus(status: SyncStatus?): String? = status?.name
+    @TypeConverter fun toSyncStatus(value: String?): SyncStatus? = value?.let { SyncStatus.valueOf(it) }
 
-    @TypeConverter
-    fun fromPaymentStatus(status: PaymentStatus?): String? = status?.name
-    @TypeConverter
-    fun toPaymentStatus(value: String?): PaymentStatus? = value?.let { PaymentStatus.valueOf(it) }
+    @TypeConverter fun fromPaymentStatus(status: PaymentStatus?): String? = status?.name
+    @TypeConverter fun toPaymentStatus(value: String?): PaymentStatus? = value?.let { PaymentStatus.valueOf(it) }
 
     // --- LIST CONVERTERS ---
-    @TypeConverter
-    fun fromOrderItemList(value: List<OrderItem>?): String = gson.toJson(value)
-    @TypeConverter
-    fun toOrderItemList(value: String): List<OrderItem>? {
+    @TypeConverter fun fromOrderItemList(value: List<OrderItem>?): String = gson.toJson(value)
+    @TypeConverter fun toOrderItemList(value: String): List<OrderItem>? {
         return gson.fromJson(value, object : TypeToken<List<OrderItem>?>() {}.type)
     }
     
-    @TypeConverter
-    fun fromPaymentList(value: List<Payment>?): String = gson.toJson(value)
-    @TypeConverter
-    fun toPaymentList(value: String): List<Payment>? {
+    @TypeConverter fun fromPaymentList(value: List<Payment>?): String = gson.toJson(value)
+    @TypeConverter fun toPaymentList(value: String): List<Payment>? {
         return gson.fromJson(value, object : TypeToken<List<Payment>?>() {}.type)
     }
     
-    // ... (other list converters can go here if needed)
+    // --- THIS IS THE FIX for the latest error ---
+    @TypeConverter fun fromModifierGroupList(value: List<ModifierGroup>?): String = gson.toJson(value)
+    @TypeConverter fun toModifierGroupList(value: String): List<ModifierGroup>? {
+        return gson.fromJson(value, object : TypeToken<List<ModifierGroup>?>() {}.type)
+    }
 }
