4f97c9ba661101dba3d614122f44321e8195a10f
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-29 13:30:25 +0500

    Update MainActivity.kt

--- android-app/app/src/main/java/com/almadina/pos/MainActivity.kt
+++ android-app/app/src/main/java/com/almadina/pos/MainActivity.kt
@@ -10,8 +10,13 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // It's a good practice to set the theme from resources before calling super.onCreate()
+        setTheme(R.style.Theme_AlMadinaPOS_NoActionBar)
+
         super.onCreate(savedInstanceState)
+        
         setContent {
+            // The Jetpack Compose theme then takes over for all the in-app UI.
             AlMadinaPOSTheme {
                 AppNavigation()
             }
